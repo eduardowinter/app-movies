4552105df0013c7c8fa0cbbbeb33ccfbb1f7065a
Eduardo Winter <eduardowb@gmail.com>
2022-10-14 15:30:15 -0300

    eslint config, api fecthing, page stylization

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import React, {useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 
-import Movie from "./components/Movie";
+import Movie from './components/Movie'
 
 const FEATURED_API =
-  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=7ecd0b11bc4cd387a22b43cb37086584";
-const IMG_API = "https://image.tmdb.org/t/p/w1280";  
-const SEARCH_API =
-  "https://api.themoviedb.org/3/search/movie?&api_key=7ecd0b11bc4cd387a22b43cb37086584&query=";
+  'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=7ecd0b11bc4cd387a22b43cb37086584'
+// const SEARCH_API =
+//   'https://api.themoviedb.org/3/search/movie?&api_key=7ecd0b11bc4cd387a22b43cb37086584&query='
 
 function App() {
-  const [movies, setMovies ] = useState([]);
+  const [movies, setMovies] = useState([])
 
   useEffect(() => {
     fetch(FEATURED_API)
-     .then((res) => res.json())
-     .then((data) => {
-      setMovies(data.results);
-     });
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.results)
+      })
   }, [])
 
-  return <div>
-      {movies.map(movie => (
-        <Movie kew={movie.id} data={movie} />
+  return (
+    <div className="movie-container">
+      {movies.map((movie) => (
+        <Movie key={movie.id} {...movie} />
       ))}
-    </div>;
+    </div>
+  )
 }
 
-export default App;
+export default App

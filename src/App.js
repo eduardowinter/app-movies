f2abff28e3dab52eda795402cb0380d899a2ee51
Eduardo Winter <eduardowb@gmail.com>
2022-10-14 14:25:35 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState } from 'react';
+
+import Movie from "./components/Movie";
+
+const FEATURED_API =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=7ecd0b11bc4cd387a22b43cb37086584";
+const IMG_API = "https://image.tmdb.org/t/p/w1280";  
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?&api_key=7ecd0b11bc4cd387a22b43cb37086584&query=";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [movies, setMovies ] = useState([]);
+
+  useEffect(() => {
+    fetch(FEATURED_API)
+     .then((res) => res.json())
+     .then((data) => {
+      setMovies(data.results);
+     });
+  }, [])
+
+  return <div>
+      {movies.map(movie => (
+        <Movie kew={movie.id} data={movie} />
+      ))}
+    </div>;
 }
 
 export default App;
---
+++ src/components/Movie.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const IMG_API = "https://image.tmdb.org/t/p/w1280";  
+
+const Movie = ({ title, poster_path, overview, vote_average }) => <div
+className="movie">This is a movie
+component</div>;
+
+export default Movie;

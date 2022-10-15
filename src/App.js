b8a44a9f2f09bcd38083274372b2fac03a9a0782
Eduardo Winter <eduardowb@gmail.com>
2022-10-14 21:34:15 -0300

    Added Search bar and connected with Search API, further stylization

--- src/App.js
+++ src/App.js
@@ -4,26 +4,58 @@ import Movie from './components/Movie'
 
 const FEATURED_API =
   'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=7ecd0b11bc4cd387a22b43cb37086584'
-// const SEARCH_API =
-//   'https://api.themoviedb.org/3/search/movie?&api_key=7ecd0b11bc4cd387a22b43cb37086584&query='
+const SEARCH_API =
+  'https://api.themoviedb.org/3/search/movie?&api_key=7ecd0b11bc4cd387a22b43cb37086584&query='
 
 function App() {
   const [movies, setMovies] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
 
   useEffect(() => {
-    fetch(FEATURED_API)
+    getMovies(FEATURED_API)
+  }, [])
+
+  const getMovies = (API) => {
+    fetch(API)
       .then((res) => res.json())
       .then((data) => {
         setMovies(data.results)
       })
-  }, [])
+  }
+
+  const handleOnSubmit = (event) => {
+    event.preventDefault()
+
+    if (searchTerm) {
+      getMovies(SEARCH_API + searchTerm)
+
+      setSearchTerm('')
+    }
+  }
+
+  const handleOnChange = (event) => {
+    setSearchTerm(event.target.value)
+  }
 
   return (
-    <div className="movie-container">
-      {movies.map((movie) => (
-        <Movie key={movie.id} {...movie} />
-      ))}
-    </div>
+    <>
+      <header>
+        <form onSubmit={handleOnSubmit}>
+          <input
+            className="search"
+            type="search"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={handleOnChange}
+          />
+        </form>
+      </header>
+      <div className="movie-container">
+        {movies.map((movie) => (
+          <Movie key={movie.id} {...movie} />
+        ))}
+      </div>
+    </>
   )
 }
 
